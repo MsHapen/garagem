9262e1ae97d30ca3ab4d69d68dab8fd9ff087cbc
MsHapen <otaviobentto@gmail.com>
2023-03-28 16:33:54 -0300

    Commit acessorio

--- Garagem/admin.py
+++ Garagem/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Categoria, Marca
+from .models import Categoria, Marca, Acessorio
 
 admin.site.register(Categoria)
 admin.site.register(Marca)
+admin.site.register(Acessorio)

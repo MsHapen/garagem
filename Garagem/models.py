ebee1e1846493c67b363ff31faf84f080ce022f7
MsHapen <otaviobentto@gmail.com>
2023-03-31 16:34:02 -0300

    Commit fInal

--- Garagem/models.py
+++ Garagem/models.py
@@ -20,18 +20,27 @@ class Acessorio(models.Model):
     def __str__(self):
         return self.descricao
     
+    class Meta:
+        verbose_name = "Acessório"
+    
 class Cor(models.Model):
     descricao = models.CharField(max_length=100)
 
     def __str__(self):
         return self.descricao
+    
+    class Meta:
+        verbose_name_plural = "Cores"
 
 class Veiculo(models.Model):
-    marca = models.ForeignKey(Veiculo, on_delete=models.PROTECT, related_name="marca")
-    categoria = models.ForeignKey(Veiculo, on_delete=models.PROTECT, related_name="categoria")
-    cor = models.ForeignKey(Veiculo, on_delete=models.PROTECT, related_name="cor")
-    ano = models.IntegerField(null=True default=0)
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculo")
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="veiculo")
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculo")
+    ano = models.IntegerField(default=0)
     preco = models.DecimalField(max_digits=10, decimal_places=2, null=True, default=0)
 
     def __str__(self):
-        return self.marca
+        return f"{self.marca} - {self.categoria} - {self.cor} - {self.ano} - {self.preco}"
+    
+    class Meta:
+        verbose_name = "Veículo"

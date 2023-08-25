3c57636d68933deb290bd4b7cf8bc6c370955701
MsHapen <otaviobentto@gmail.com>
2023-08-25 10:53:03 -0300

    tentando fazer o L

--- config/settings.py
+++ config/settings.py
@@ -57,10 +57,14 @@ MIDDLEWARE = [
      
 ]
 REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.DjangoModelPermissions",
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        
+        
     ]
 }
 SPECTACULAR_SETTINGS = {
